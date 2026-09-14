6fa1163a7c24beeeb03689fbed3a291acc60252f
pfeatherstone <pfeatherstone@pf>
2026-09-14 15:03:51 +0100

    fail-fast = false on macos. Make FindMDAZ.cmake work on AppleClang

--- cmake/FindMDAZ.cmake
+++ cmake/FindMDAZ.cmake
@@ -1,13 +1,20 @@
 include(FindPackageHandleStandardArgs)
-include(CheckCXXCompilerFlag)
-include(CheckLinkerFlag)
+include(CheckCSourceCompiles)
+set(CMAKE_REQUIRED_QUIET_SAV ${CMAKE_REQUIRED_QUIET})
+set(CMAKE_REQUIRED_FLAGS_SAV ${CMAKE_REQUIRED_FLAGS})
+set(CMAKE_REQUIRED_QUIET TRUE)
+set(CMAKE_REQUIRED_FLAGS "-mdaz-ftz -Werror")
 
-check_cxx_compiler_flag("-mdaz-ftz" HAS_MDAZ_FTZ_COMPILE)
-check_linker_flag(CXX "-mdaz-ftz" HAS_MDAZ_FTZ_LINK)
+check_c_source_compiles(
+  "int main() { return 0; }"
+  HAS_MDAZ_FTZ
+)
 
-if(HAS_MDAZ_FTZ_COMPILE AND HAS_MDAZ_FTZ_LINK)
-    set(MDAZ_CFLAGS  "-mdaz-ftz")
-    set(MDAZ_LDFLAGS "-mdaz-ftz")
+if(HAS_MDAZ_FTZ)
+  set(MDAZ_CFLAGS "-mdaz-ftz")
+  set(MDAZ_LDFLAGS "-mdaz-ftz")
 endif()
 
+set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_SAV})
+set(CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS_SAV})
 find_package_handle_standard_args(MDAZ DEFAULT_MSG MDAZ_CFLAGS MDAZ_LDFLAGS)
